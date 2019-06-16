edfd954e84f582bd3cfbfb71b1f31c1c764b6e65
Nikolaos Papadakis <nipapadak@gmail.com>
2019-06-16 11:31:30 -0400

    - Drawing Paths and Shapes Tutorial

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -1,9 +1,9 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view showing the details for a landmark.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view showing the details for a landmark.
+ */
 
 import SwiftUI
 
@@ -20,51 +20,52 @@ struct LandmarkDetail: View {
             MapView(coordinate: landmark.locationCoordinate)
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
-
-            CircleImage(image: landmark.image(forSize: 50))
+            
+            CircleImage(image: landmark.image(forSize: 250))
                 .offset(x: 0, y: -130)
                 .padding(.bottom, -130)
-
+            
             VStack(alignment: .leading) {
-                Text(landmark.name)
-                    .font(.title)
-                
-                Button(action: {
-                    self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
-                }) {
-                    if self.userData.landmarks[self.landmarkIndex].isFavorite {
-                        Image(systemName: "star.fill")
-                            .foregroundColor(Color.yellow)
-                    } else {
-                        Image(systemName: "star")
-                            .foregroundColor(Color.gray)
+                HStack {
+                    Text(verbatim: landmark.name)
+                        .font(.title)
+                    
+                    Button(action: {
+                        self.userData.landmarks[self.landmarkIndex]
+                            .isFavorite.toggle()
+                    }) {
+                        if self.userData.landmarks[self.landmarkIndex]
+                            .isFavorite {
+                            Image(systemName: "star.fill")
+                                .foregroundColor(Color.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .foregroundColor(Color.gray)
+                        }
                     }
                 }
                 
                 HStack(alignment: .top) {
-                    Text(landmark.park)
-                        .font(.caption)
+                    Text(verbatim: landmark.park)
+                        .font(.subheadline)
                     Spacer()
-                    Text(landmark.state)
-                        .font(.caption)
+                    Text(verbatim: landmark.state)
+                        .font(.subheadline)
                 }
-            }
-            .padding()
-
+                }
+                .padding()
+            
             Spacer()
         }
-        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
     }
 }
 
 #if DEBUG
-struct LandmarkDetail_Previews: PreviewProvider {
+struct LandmarkDetail_Preview: PreviewProvider {
     static var previews: some View {
-        Group {
-            LandmarkDetail(landmark: landmarkData[0])
-            LandmarkDetail(landmark: landmarkData[1])
-        }
-        .environmentObject(UserData())
+        let userData = UserData()
+        return LandmarkDetail(landmark: userData.landmarks[0])
+            .environmentObject(userData)
     }
 }
 #endif
---
+++ Landmarks/SupportingViews/RotatedBadgeSymbol.swift
@@ -0,0 +1,27 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by Nikolaos Papadakis on 6/16/19.
+//  Copyright © 2019 Optimal Mobile Solutions Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol : View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+#if DEBUG
+struct RotatedBadgeSymbol_Previews : PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: .init(degrees: 5))
+    }
+}
+#endif
